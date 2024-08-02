a9b1049ed5ffb41cb643a9dff2e254ce4346647d
Carlos6464 <adrianosodre23@gmail.com>
2024-08-01 22:27:12 -0300

    chore: adding error handling

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,28 @@
 import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+import cors from "cors"
 import router from "./router";
 
 const app = express();
 const port = 3333;
 
-app.use(express.json);
+app.use(express.json());
+app.use(cors());
+
 app.use(router);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if(error instanceof Error){
+        return res.status(400).json({
+            error: error.message
+        })
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    })
+})
 
 app.listen(port, () => {
     console.log("Servidor Online!");
